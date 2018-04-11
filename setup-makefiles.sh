2a40076b5149f75abb0d1c78fa1a4e540975df34
martinusbe <martinusbe@gmail.com>
2018-04-11 09:52:33 +0200

    berkeley: adjust extract utils for gzosp

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,9 +25,9 @@ VENDOR=huawei
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+GZOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$GZOSP_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -35,7 +35,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$GZOSP_ROOT"
 
 # Copyright headers and guards
 write_headers
