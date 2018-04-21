1199707cd35fc2653d6245b1bb8c16e9f5d21ca6
hardwareadictos <hardwareadictos@gmail.com>
2018-04-21 16:34:00 +0200

    berkeley: ValidusOS-ify

--- validus.mk
+++ validus.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 Ground Zero Roms
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,10 +17,10 @@
 
 $(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ValidusOS stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_berkeley
+PRODUCT_NAME := validus_berkeley
 PRODUCT_DEVICE := berkeley
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Honor View 10
