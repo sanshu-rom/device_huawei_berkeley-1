1a2e08748e5ec0089d702d3c4c34294dcf792c61
Chris <chris@cypheros.co>
2018-04-25 16:25:14 -0400

    berkeley: Enable vendor property overrides
    Small workaround to display our own strings for the devices model, build
    number and security patch level. Since berkeley's vendor props override
    the system ones, we can dictate what specific props the about phone menu
    reads from instead.
    
    Change-Id: I68599fec6d938bc59039e88557a9b622e7113228

--- device.mk
+++ device.mk
@@ -77,6 +77,11 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.release=$(PLATFORM_VERSION) \
     ro.build.version.sdk=$(PLATFORM_SDK_VERSION) \
     ro.cust.cdrom=/dev/null
+# Vendor Properties Overrides
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.vendor.override.security_patch=$(PLATFORM_SECURITY_PATCH) \
+    ro.vendor.override.build_display=$(BUILD_ID)
+
 
 # Release tools
 PRODUCT_COPY_FILES += \
