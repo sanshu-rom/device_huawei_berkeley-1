79eb1d90f4ee5e607252f8c677b22b05fd808594
Paul Keith <javelinanddart@gmail.com>
2018-05-09 02:21:19 +0200

    berkeley: Implement a dummy IExtTelephony service
    * To implement manual SIM provisioning
    
    Change-Id: I9bca42c0d7d5d60f3cc96f6a4b75ad569e9a787f

--- device.mk
+++ device.mk
@@ -77,15 +77,18 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.release=$(PLATFORM_VERSION) \
     ro.build.version.sdk=$(PLATFORM_SDK_VERSION) \
     ro.cust.cdrom=/dev/null
+
 # Vendor Properties Overrides
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.override.security_patch=$(PLATFORM_SECURITY_PATCH) \
     ro.vendor.override.build_display=$(BUILD_ID)
-    
+
 # Radio
 PRODUCT_PACKAGES += \
     qti-telephony-common
 
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Release tools
 PRODUCT_COPY_FILES += \
